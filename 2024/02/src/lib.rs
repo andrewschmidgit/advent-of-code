012275bade1cd8735a4a331c3a89c1d100dd4fa6
Andrew Schmid <andrewschmid-dev@proton.me>
2024-12-03 15:32:20 -0600

    day 2 pt 2

--- 2024/02/src/lib.rs
+++ 2024/02/src/lib.rs
@@ -1,14 +1,26 @@
 use std::error::Error;
 
+use itertools::Itertools;
 use report::Report;
 mod report;
 
 pub fn run(contents: &str) -> Result<(), Box<dyn Error>> {
     let reports = parse(contents)?;
 
-    let safe_reports = reports.iter().filter(|r| r.is_safe()).count();
+    let safe_reports = reports.iter().filter(|r| check_safety(&r.levels)).count();
 
-    println!("safe reports: {}", safe_reports);
+    println!("safe reports: {}/{}", safe_reports, reports.len());
+
+    let tolerant_safe_reports = reports
+        .iter()
+        .filter(|r| check_safety_tolerant(&r.levels))
+        .count();
+
+    println!(
+        "safe reports with fault tolerance: {}/{}",
+        tolerant_safe_reports,
+        reports.len()
+    );
 
     Ok(())
 }
@@ -20,5 +32,78 @@ fn parse(contents: &str) -> Result<Vec<Report>, Box<dyn Error>> {
         .collect::<Result<Vec<Report>, _>>()?)
 }
 
-#[test]
-fn parse_succeeds() {}
+fn check_safety(levels: &Vec<i32>) -> bool {
+    // true is increasing, false is decreasing
+    let mut direction: Option<bool> = None;
+
+    for pair in levels.windows(2) {
+        let a = pair[0];
+        let b = pair[1];
+
+        let dir = b > a;
+        let diff = (b - a).abs();
+
+        if let Some(direction) = direction {
+            if dir != direction {
+                return false; // unsafe, direction has changed
+            }
+        } else {
+            direction = Some(dir);
+        }
+
+        if diff < 1 || diff > 3 {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn check_safety_tolerant(levels: &Vec<i32>) -> bool {
+    levels
+        .iter()
+        .copied()
+        .combinations(levels.len() - 1)
+        .any(|c| check_safety(&c))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn report_safety() {
+        let cases = vec![
+            ("7 6 4 2 1", true),
+            ("1 2 7 8 9", false),
+            ("9 7 6 2 1", false),
+            ("1 3 2 4 5", false),
+            ("8 6 4 4 1", false),
+            ("1 3 6 7 9", true),
+        ];
+
+        for (input, is_safe) in cases {
+            println!("input: {}, expected safety: {}", input, is_safe);
+            let report: Report = input.parse().unwrap();
+            assert_eq!(is_safe, check_safety(&report.levels));
+        }
+    }
+
+    #[test]
+    fn report_tolerant_safety() {
+        let cases = vec![
+            ("7 6 4 2 1", true),
+            ("1 2 7 8 9", false),
+            ("9 7 6 2 1", false),
+            ("1 3 2 4 5", true),
+            ("8 6 4 4 1", true),
+            ("1 3 6 7 9", true),
+        ];
+
+        for (input, is_safe) in cases {
+            println!("input: {}, expected safety: {}", input, is_safe);
+            let report: Report = input.parse().unwrap();
+            assert_eq!(is_safe, check_safety_tolerant(&report.levels));
+        }
+    }
+}
--- 2024/02/src/report.rs
+++ 2024/02/src/report.rs
@@ -2,36 +2,7 @@ use std::{num::ParseIntError, str::FromStr};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Report {
-    levels: Vec<i32>,
-}
-
-impl Report {
-    pub fn is_safe(self: &Self) -> bool {
-        // true is increasing, false is decreasing
-        let mut direction: Option<bool> = None;
-
-        for pair in self.levels.windows(2) {
-            let a = pair[0];
-            let b = pair[1];
-
-            let dir = b > a;
-            let diff = (b - a).abs();
-
-            if let Some(direction) = direction {
-                if dir != direction {
-                    return false; // unsafe, direction has changed
-                }
-            } else {
-                direction = Some(dir);
-            }
-
-            if diff < 1 || diff > 3 {
-                return false;
-            }
-        }
-
-        true
-    }
+    pub levels: Vec<i32>,
 }
 
 impl FromStr for Report {
@@ -61,23 +32,5 @@ mod tests {
         let report: Report = input.parse().unwrap();
 
         assert_eq!(report, expected);
-    }
-
-    #[test]
-    fn report_safety() {
-        let cases = vec![
-            ("7 6 4 2 1", true),
-            ("1 2 7 8 9", false),
-            ("9 7 6 2 1", false),
-            ("1 3 2 4 5", false),
-            ("8 6 4 4 1", false),
-            ("1 3 6 7 9", true),
-        ];
-
-        for (input, is_safe) in cases {
-            println!("input: {}, expected safety: {}", input, is_safe);
-            let report: Report = input.parse().unwrap();
-            assert_eq!(is_safe, report.is_safe());
-        }
     }
 }
