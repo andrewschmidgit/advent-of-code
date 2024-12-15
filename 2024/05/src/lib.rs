523a9a57fb8750c7bff9b7b431fe8585692fbe9c
Andrew Schmid <andrewschmid-dev@proton.me>
2024-12-14 19:07:18 -0600

    day 5 pt 2

--- 2024/05/src/lib.rs
+++ 2024/05/src/lib.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-
-use rule::RuleSet;
+use std::{cmp::Ordering, error::Error, str::FromStr};
 
 mod rule;
 
@@ -9,8 +7,12 @@ pub fn run(contents: &str) -> Result<(), Box<dyn Error>> {
         .split_once("\n\n")
         .ok_or("expected double newline")?;
 
-    let ruleset: RuleSet = rules.parse()?;
-    let updates: Vec<Vec<usize>> = updates
+    let rules = rules
+        .lines()
+        .map(|l| l.parse())
+        .collect::<Result<Vec<Rule>, _>>()?;
+
+    let mut updates: Vec<Vec<usize>> = updates
         .lines()
         .map(|l| {
             l.split(",")
@@ -19,31 +21,97 @@ pub fn run(contents: &str) -> Result<(), Box<dyn Error>> {
         })
         .collect::<Result<Vec<Vec<usize>>, _>>()?;
 
-    let mut middle_page_sum = 0;
+    let mut ordered_middle_sum = 0;
 
-    for update in updates {
-        let mut ruleset = ruleset.clone();
-        let mut is_update_ordered = true;
+    for update in updates.iter_mut() {
+        println!("checking update: {:?}", update);
 
-        for n in update.iter() {
-            let tripped = ruleset.evaluate(*n);
-
-            if tripped {
-                println!("{} violated a rule", n);
-                is_update_ordered = false;
-                break;
+        let mut violations: Vec<Violation> = vec![];
+        for rule in rules.iter() {
+            if let Some(violation) = rule.evaluate(&update) {
+                violations.push(violation);
             }
         }
 
-        if is_update_ordered {
-            let middle_index = (update.len() - 1) / 2;
-            middle_page_sum += update.get(middle_index).unwrap();
-
-            println!("update {:?} is correctly ordered", update);
+        if violations.len() == 0 {
+            ordered_middle_sum += update[(update.len() - 1) / 2];
         }
     }
 
-    println!("middle page number sum: {}", middle_page_sum);
+    let order = |a: &usize, b: &usize| -> Ordering {
+        if rules.contains(&Rule {
+            left: *a,
+            right: *b,
+        }) {
+            Ordering::Less
+        } else if rules.contains(&Rule {
+            left: *b,
+            right: *a,
+        }) {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    };
+
+    let mut sum = 0;
+    for update in updates.iter_mut() {
+        if !update.is_sorted_by(|a, b| order(a, b) != Ordering::Greater) {
+            update.sort_by(order);
+            sum += update[update.len() / 2];
+        }
+    }
+
+    println!("ordered sum: {}", ordered_middle_sum);
+    println!("sum: {}", sum);
 
     Ok(())
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Rule {
+    left: usize,
+    right: usize,
+}
+
+impl Rule {
+    pub fn evaluate(&self, list: &[usize]) -> Option<Violation> {
+        let mut right_index: Option<usize> = None;
+
+        for (i, el) in list.iter().enumerate() {
+            if let Some(right_index) = right_index {
+                if *el == self.left {
+                    return Some(Violation {
+                        left_index: i,
+                        right_index,
+                    });
+                }
+            } else if *el == self.left {
+                return None;
+            } else if *el == self.right {
+                right_index = Some(i);
+            }
+        }
+
+        None
+    }
+}
+
+impl FromStr for Rule {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (left, right) = s.split_once("|").ok_or("could not fine a '|'")?;
+
+        Ok(Self {
+            left: left.parse()?,
+            right: right.parse()?,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Violation {
+    left_index: usize,
+    right_index: usize,
 }
